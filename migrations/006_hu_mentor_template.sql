090889a019c146354ccc9a011d3b395798c26bbd
Johnhu <Johnho2929@gmail.com>
2026-06-17 00:07:40 +0800

    fix: 移除自建檔案，完全還原至組員版本

--- migrations/006_hu_mentor_template.sql
+++
@@ -1,9 +0,0 @@
--- 006_hu_mentor_template.sql
--- 新增 mentor_type 欄位 (odd, even, all)，預設為 all
-ALTER TABLE `teachers`
-ADD COLUMN `mentor_type` ENUM('odd', 'even', 'all') DEFAULT 'all' COMMENT '導師單雙號類型';
-
--- 測試資料：將老師 a1125525 (蟹從峰) 設為雙數導師
-UPDATE `teachers` SET `mentor_type` = 'even' WHERE `username` = 'a1125525';
--- 測試資料：將老師 A11255255 (薛從峰) 設為單數導師
-UPDATE `teachers` SET `mentor_type` = 'odd' WHERE `username` = 'A11255255';
